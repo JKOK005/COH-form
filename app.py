572a3cd0fe99a702cc5177344d44355baf10eb5f
JKOK005 <JKOK005@e.ntu.edu.sg>
2019-05-04 15:42:55 +0800

    Touching up main form

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def render_form_template():
-	return render_template('form.html')
+	if request.method == "GET":
+		app.logger.debug("Rendering main form")
+		return render_template('form.html')
+	else: 
+		abort(404)
 
 @app.route('/heart-beat')
 def heart_beat():
